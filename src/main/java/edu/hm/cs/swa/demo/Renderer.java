92e1b5969403ff5f9690c94b9c4c2a7825a32bc6
Ghouz <sven.schatter@gmail.com>
2017-04-03 14:44:47 +0200

    added Renderer

---
+++ src/main/java/edu/hm/cs/swa/demo/Renderer.java
@@ -0,0 +1,14 @@
+package edu.hm.cs.swa.demo;
+
+public class Renderer {
+
+    private final Object objectToRender;
+
+    public Renderer(Object objectToRender) {
+        this.objectToRender = objectToRender;
+    }
+
+    public String render(){
+        return "";
+    }
+}
