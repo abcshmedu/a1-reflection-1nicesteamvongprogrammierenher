47fbe779306d55e8e372c563e8f4f1f0b16182c3
Sven Schatter <sven.schatter@gmail.com>
2017-04-08 22:21:13 +0200

    restructering of package

--- src/main/java/edu/hm/renderer/ArrayRenderer.java
+++ src/main/java/edu/hm/renderer/ArrayRenderer.java
@@ -1,4 +1,16 @@
 package edu.hm.renderer;
 
 public class ArrayRenderer {
+    private static final String INT_ARRAY_TEMPLATE = "(Type int[]): [%s]\n";
+
+    public String render(int[] array) {
+        final StringBuilder arrayDataBuilder = new StringBuilder();
+
+        for (int integer : array) {
+            arrayDataBuilder.append(integer);
+            arrayDataBuilder.append(", ");
+        }
+
+        return String.format(INT_ARRAY_TEMPLATE, arrayDataBuilder.toString());
+    }
 }
